6992332dbb6502e6490ef4bf2f68cd32accc1b3b
8scarletrose813 <8scarlet.rose813@gamil.com>
2026-03-08 02:53:03 +0200

    Add folder frontend and first settings messanger

---
+++ source/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <h1>Leaves</h1>
+    <nav class="header">
+        <h3><a href="#">Log up</a></h3>
+        <p>|</p>
+        <h3><a href="#"> Log in</a></h3>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
