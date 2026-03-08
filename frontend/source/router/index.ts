5de78f977bbf1fb31915d1bb06e8210d3b809439
DrBopoH <lecrowz666@gmail.com>
2026-03-09 00:12:07 +0200

    Brake Changes: Global fs refactoring and frontend files renaming

--- frontend/source/router/index.ts
+++ frontend/source/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import IndexView from '../views/IndexView.vue'
-import SignupView from '../views/SignupView.vue'
-import SigninView from '../views/SigninView.vue'
+import IndexView from '../pages/LandingPage.vue'
+import SignupView from '../pages/SignupPage.vue'
+import SigninView from '../pages/SigninPage.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
