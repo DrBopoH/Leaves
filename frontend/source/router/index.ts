b06b47fb684fef80221ebcfea78f89eb175ea7ab
DrBopoH <lecrowz666@gmail.com>
2026-03-09 08:35:22 +0200

    Pre-release v0.a1.28

--- frontend/source/router/index.ts
+++ frontend/source/router/index.ts
@@ -7,7 +7,7 @@ import AppPage from '../pages/AppPage.vue'
 
 const router = createRouter(
     {
-        history: createWebHistory(import.meta.env.BASE_URL),
+        history: createWebHashHistory(import.meta.env.BASE_URL),
         routes: [
             { path: '/',       name: 'landing', component: LandingPage },
             { path: '/auth', name: 'auth', component: AuthPage },
